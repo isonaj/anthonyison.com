a6d77879b65f37793cf37d5adb66d0fb2dc3d73d
Anthony <isonaj@gmail.com>
2019-07-31 13:53:50 +1000

    Output to ./public and added feed

--- source/.vuepress/config.js
+++ source/.vuepress/config.js
@@ -2,7 +2,11 @@ module.exports = {
   title: 'Anthony Ison',
   description: 'Software development and DevOps. Bringing solutions to life.',
   theme: 'isonaj',
+  dest: './public',
   themeConfig: {
-    
+    feed: {
+      canonical_base: 'https://anthonyison.com',
+    }
+      
   }
 }  
